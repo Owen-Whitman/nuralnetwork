a0d534a34ddff81b12ef3f0249bea5554c152421
Owen 22. Whitman <o22whitm@sea0078.saes.org>
2019-04-22 08:44:51 -0400

    orginal

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import random
 inputs = [[1, 2], [0, 1]]
 ans = [[[inputs[0]]]]
 completed = []
-num_ofnet = [2]
+num_ofnet = [2,2]
 for every in num_ofnet:
     ans[0].append([])
 right = [[1,2],[3,4]]
@@ -49,68 +49,6 @@ def create_network(numeachlayer):
             ans[0][item][0] = stat(ans[0][item])
             ans[0][item + 1].append(ans[0][item][0])
 
-def calc_cost(x,a,t):
-  if t == "t":
-    calc = 0
-    calc = (math.sqrt((x-a) ** 2)/2)
-    return calc
-  else:
-    calc = 0
-    for every in x:
-      index = x.index(every)
-      calc += (math.sqrt((every-a[index]) ** 2)/2)
-    return calc
-
-def sloping(x):
-  print(x)
-  
-  for e in range(0,len(ans[-1]),2):
-    for v in range(len(ans[-1][e][-1][0])):
-      try:
-        print(v,e[-3][0][v])
-      except:
-        num = 0
-        #print(x[index][v],inputs[index][v])
-        num = (x[e][v]-x[e+1][v])/(inputs[e][v]-inputs[e+1][v])
-        print(num, "slope")
-  print(ans[-1])
-'''
-def learn():
-  cost = 0
-  deltaC = []
-  deltaCs = []
-  deltaCb = []
-  changednums = []
-  learningrate = 0.1
-  for er in output:
-    index = output.index(er)
-    cost += calc_cost(er,right[index],"b")
-  for e in range(len(ans)):
-    deltaC.append([])
-    deltaCs.append([])
-    deltaCb.append([])
-    for ever in ans[e][-2]:
-      if ever != ans[e][-2][0]:
-        index = ans[e][-2].index(ever)
-        deltaC.append([])
-        print(ever)
-        deltaC[index-1].append(calc_cost(ever[-1],right[e][index-1],"t"))
-      else:
-        for b in ever:
-          deltaCs[index-1].append(calc_cost(b,right[e][index-1],"t"))
-          deltaCb[index-1].append(calc_cost([0,right[e][index-1],"f"))
-  print(deltaCb, "aing")
-  print(deltaCs,"dabs")
-  ans.append(list(ans))
-  for c in range(len(deltaC)-1,0,-1):
-    if(deltaC[c] == []):
-      deltaC.pop(c)
-  for uno in range(len(deltaC)):
-    changednums.append([])
-    for dos in range(len(deltaC[uno])):
-      changednums[uno].append(ans[uno][-1][0][dos]-learningrate*deltaC[uno][dos])'''
-
-
 def runnet(x):
     index = 0
     every = 0
@@ -128,17 +66,6 @@ def runnet(x):
 
 def main():
     create_network(num_ofnet)
-
-    '''for everu in range(1, len(inputs)):
-        runnet(everu)
-    for ev in range(len(ans)):
-        output.append(ans[ev][-1][0])
-
-    for every in ans:
-      print(every)
-      print(" ")
-    learn()'''
-    # fitness.append(calc_cost(ans[-1][0],0, 0.1))
 
 
 main()
